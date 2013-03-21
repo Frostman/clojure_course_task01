a5d4af0a62b7b76e804af0b5923a409086fbc773
Sergey Lukjanov <me@frostman.ru>
2013-03-21 12:33:34 +0400

    cleanup

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -8,11 +8,10 @@
     (= (:class (attributes n)) "l")))
 
 (defn get-links []
-  (let [data (parse "clojure_google.html")]
-    (map #(:href (attributes %))
-      (filter search-result?
-        (tree-seq #(keyword? (tag %)) children
-          (parse "clojure_google.html"))))))
+  (map #(:href (attributes %))
+    (filter search-result?
+      (tree-seq #(keyword? (tag %)) children
+        (parse "clojure_google.html")))))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
